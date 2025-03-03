41e2969dd57da6748c8a7dea1c0534c22311c964
imrany <imranmat254@gmail.com>
2025-03-03 20:59:14 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,59 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+    <div className="flex flex-col h-screen items-center justify-center pb-20 font-[family-name:var(--font-geist-sans)]  bg-gradient-to-t from-blue-100/20 dark:from-blue-900/5">
+      <svg aria-hidden="true" className="pointer-events-none [z-index:-1] absolute inset-0 h-full w-full fill-blue-500/50 stroke-blue-500/50 [mask-image:linear-gradient(to_top,_#ffffffad,_transparent)] opacity-[.30]" style={{visibility: "visible"}}>
+        <defs>
+          <pattern id=":Rs57qbt6ja:" width="20" height="20" patternUnits="userSpaceOnUse" x="-1" y="-1">
+            <path d="M.5 20V.5H20" fill="none" strokeDasharray="0"></path>
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" strokeWidth="0" fill="url(#:Rs57qbt6ja:)"></rect>
+      </svg>
+
+      <main className="flex flex-col gap-y-12 justify-center h-full items-center">
+        <h1 className="text-4xl sm:text-6xl leading-0.5 tracking-tighter w-full text-zinc-700 font-bold text-center sm:text-left">
+          The Best Screener for your resume
+        </h1>
+        <p className="text-gray-500 text-lg text-wrap text-center w-[720px]">
+          Find online job offers by uploading or typing your resume and qualification here,
+          We use <span className="text-black font-semibold">artificial intelligence</span> to scan and scrap your resume and find you online job offers.
+        </p>
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        /> */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 w-full justify-center items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="flex items-center justify-center h-[40px] w-[150px] bg-black text-white rounded-[10px]"
+            href="/"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Get Started
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="flex items-center justify-center h-[40px] w-[150px] border-[1px] rounded-[10px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Type Resume
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
         >
           <Image
             aria-hidden
@@ -63,13 +58,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Type resume
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
         >
           <Image
             aria-hidden
@@ -82,9 +75,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
         >
           <Image
             aria-hidden
@@ -93,7 +84,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to dashboard →
         </a>
       </footer>
     </div>
